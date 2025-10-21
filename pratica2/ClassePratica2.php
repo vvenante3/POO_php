6d679ce4edece2b4c441d99af7bc2aa9163fe220
VitorVenante <vitor.venante@hotmail.com>
2025-10-21 19:54:37 -0300

    estrutura da classe pratica2

--- pratica2/ClassePratica2.php
+++ pratica2/ClassePratica2.php
@@ -4,30 +4,79 @@ Class Carro {
     public $marca;
     public $modelo;
     public $cor;
+    public $ligado;
+    public $desligado;
     private $motor;
     private $motorista;
+    private $dirigir;
 
     public function __construct()
     {
         
     }
 
-    public function Ligar()
-    {
-
+    public function getMarca(){
+        return $this->marca;
     }
 
-    public function Desligar()
-    {
+    public function setMarca($marca){
+        $this->marca = $marca;
+    } 
 
+    public function getModelo(){
+        return $this->modelo;
     }
 
-    public function Dirigir()
+    public function setModelo($modelo){
+       $this->modelo = $modelo; 
+    }
+
+    public function getLigar() 
     {
-        
+        return $this->ligado;
+    }
+
+    public function setLigar($ligado)
+    {
+        $this->ligado = $ligado;
+    }
+
+    public function getDesligar()
+    {
+        return $this->desligado;
+    }
+
+    public function setDesligar($desligado)
+    {
+        $this->desligado = $desligado;
+    }
+
+    public function getMotor(){
+        return $this->motor;
+    }
+
+    public function setMotor($motor){
+        $this->motor = $motor;
+    }
+
+    public function getMotorista(){
+        return $this->motorista;
+    }
+
+    public function setMotorista($motorista){
+        $this->motorista = $motorista;
+    }
+
+    public function getDirigir()
+    {
+        return $this->dirigir;
+    }
+
+    public function setDirigir($dirigir)
+    {
+        $this->dirigir = $dirigir;
     }
 
 }
 
-    
 ?>
