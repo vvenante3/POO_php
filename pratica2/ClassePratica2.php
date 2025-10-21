1efed20880be5ea24afd1418b4c313fd21757997
Vitor Venante <vitorvenante@vlupt.com>
2025-10-21 16:36:59 -0300

    pratica2 poo

---
+++ pratica2/ClassePratica2.php
@@ -0,0 +1,33 @@
+<?php
+
+Class Carro {
+    public $marca;
+    public $modelo;
+    public $cor;
+    private $motor;
+    private $motorista;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function Ligar()
+    {
+
+    }
+
+    public function Desligar()
+    {
+
+    }
+
+    public function Dirigir()
+    {
+        
+    }
+
+}
+
+    
+?>
