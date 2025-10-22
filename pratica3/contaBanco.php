ef2ad027397b4e65d5093e93dcbecd911f102de0
VitorVenante <vitor.venante@hotmail.com>
2025-10-22 20:41:34 -0300

    pratica 3 finalizada

--- pratica3/contaBanco.php
+++ pratica3/contaBanco.php
@@ -9,9 +9,31 @@
     <?php
         require_once 'classeBanco.php';
 
+        $pessoa1 = new ContaBanco();
+        $pessoa2 = new ContaBanco();
 
+        $pessoa1->setDono("Jubileu");
+        $pessoa1->abrirConta("CC");
+        $pessoa1->setnumConta(123);
+
+            $pessoa1->depositar(300);
+            $pessoa1->pagarMensal();
+
+            $pessoa1->fecharConta();
         
 
+        $pessoa2->setDono("Creuza");
+        $pessoa2->abrirConta("CP");
+        $pessoa2->setnumConta(321);
+
+            $pessoa2->depositar(500);
+            $pessoa2->sacar(100);
+            $pessoa2->pagarMensal();
+
+            $pessoa2->fecharConta();
+
+        var_dump($pessoa1);
+        var_dump($pessoa2);
 
     ?>
 </body>
