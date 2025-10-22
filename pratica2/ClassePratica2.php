bfe0e31348b769cdc11b351c4258a7e7121896ad
Vitor Venante <vitorvenante@vlupt.com>
2025-10-22 15:37:04 -0300

    pratica 2 - refeito

--- pratica2/ClassePratica2.php
+++ pratica2/ClassePratica2.php
@@ -1,80 +1,46 @@
 <?php
 
 Class Carro {
+
     public $marca;
     public $modelo;
     public $cor;
     public $ligado;
-    public $desligado;
-    private $motor;
-    private $motorista;
-    private $dirigir;
 
-    public function __construct()
+    public function __construct($mar, $mod, $c)
     {
-        
+        $this->marca = $mar;
+        $this->modelo = $mod;
+        $this->cor = $c;
+        $this->Ligar();
+    }
+
+    public function Ligar(){
+        $this->ligado = true;
     }
 
     public function getMarca(){
         return $this->marca;
     }
 
-    public function setMarca($marca){
-        $this->marca = $marca;
-    } 
+    public function setMarca($mar){
+        $this->marca = $mar;
+    }
 
     public function getModelo(){
         return $this->modelo;
     }
 
-    public function setModelo($modelo){
-       $this->modelo = $modelo; 
+    public function setModelo($mod){
+        $this->modelo = $mod;
     }
 
-    public function getLigar() 
-    {
-        return $this->ligado;
+    public function getCor(){
+        return $this->cor;
     }
-
-    public function setLigar($ligado)
-    {
-        $this->ligado = $ligado;
-    }
-
-    public function getDesligar()
-    {
-        return $this->desligado;
-    }
-
-    public function setDesligar($desligado)
-    {
-        $this->desligado = $desligado;
-    }
-
-    public function getMotor(){
-        return $this->motor;
-    }
-
-    public function setMotor($motor){
-        $this->motor = $motor;
-    }
-
-    public function getMotorista(){
-        return $this->motorista;
-    }
-
-    public function setMotorista($motorista){
-        $this->motorista = $motorista;
-    }
-
-    public function getDirigir()
-    {
-        return $this->dirigir;
-    }
-
-    public function setDirigir($dirigir)
-    {
-        $this->dirigir = $dirigir;
+    
+    public function setCor($c){
+        $this->cor = $c;
     }
 
 }
--- pratica2/Pratica2.php
+++ pratica2/Pratica2.php
@@ -6,6 +6,16 @@
     <title>Pratica 2 - POO</title>
 </head>
 <body>
+    <?php
+    require_once 'ClassePratica2.php';
+
+    $carro1 = new Carro("Ford", "Ka", "Prata");
+
+    // $carro1->setModelo("Chevrolet"); Prevalece sobre o modelo na linha 12
+
+    print_r($carro1);
     
+
+    ?>
 </body>
 </html>
