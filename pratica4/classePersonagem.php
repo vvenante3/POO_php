6e4aba63ab4e636b3efbd3039128fc20922a2250
VitorVenante <vitor.venante@hotmail.com>
2025-10-23 21:49:28 -0300

    preparacao pratica 4

---
+++ pratica4/classePersonagem.php
@@ -0,0 +1,43 @@
+<?php
+
+class Personagem
+{
+    // atributos
+    private  string  $nome;
+    private  string  $classe;
+    private  int     $nivel;
+    private  int     $vida;
+    private  int     $magia;
+    private  bool    $vivo;
+
+    // métodos
+    public function __construct()
+    {
+        
+    }
+
+    public function atacar()
+    {
+        
+    }
+
+    public function descansar()
+    {
+
+    }
+
+    public function subirNivel()
+    {
+
+    }
+
+    public function status()
+    {
+
+    }
+
+    
+
+}
+
+?>
