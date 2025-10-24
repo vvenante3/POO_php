3607dd09baf61a9bab532c82c9df783df55b753a
Vitor Venante <vitorvenante@vlupt.com>
2025-10-24 16:26:21 -0300

    pratica 4 em andamento

--- pratica4/classePersonagem.php
+++ pratica4/classePersonagem.php
@@ -3,40 +3,111 @@
 class Personagem
 {
     // atributos
-    private  string  $nome;
-    private  string  $classe;
+    private  ?string  $nome     = null;
+    private  ?string  $classe   = null;
     private  int     $nivel;
     private  int     $vida;
-    private  int     $magia;
+    private  int     $forca;
     private  bool    $vivo;
 
     // métodos
     public function __construct()
     {
+        $this->nivel    = 0;
+        $this->vida     = 100;
+        $this->forca    = 100;
+        $this->vivo     = true;
+    }
+
+    // public function atacar()        
+    // {
         
-    }
+    // }
 
-    public function atacar()
-    {
-        
-    }
-
-    public function descansar()
+    public function dano()          // diminui a vida do personagem com base na força (-10% da força(atributo a ser passado))
     {
 
     }
 
-    public function subirNivel()
+    public function death()        // se vida menor que 0, $vivo = false; 
     {
 
     }
 
-    public function status()
+    public function descansar()     // se vida < 100, recupera 10 de vida
     {
-
+        if ($this->getVida() < 100) {
+            $this->setVida($this->getVida() + 10);
+            echo "<p>Vida recuperada +10.</p>";
+        } else {
+            echo "<p>Vida cheia. Sem possiblidade de descansar.</p>";
+        }
     }
 
-    
+    public function subirNivel()    // aumenta nivel de 1 em 1
+    {
+       $this->setNivel($this->getNivel() + 1);
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    public function setClasse($classe)
+    {
+        $this->classe = $classe;
+    }
+
+    public function getNivel()
+    {
+        return $this->nivel;
+    }
+
+    public function setNivel($nivel)
+    {
+        $this->nivel = $nivel;
+    }
+
+    public function getVida()
+    {
+        return $this->vida;
+    }
+
+    public function setVida($vida)
+    {
+        $this->vida = $vida;
+    }
+
+    public function getForca()
+    {
+        return $this->forca;
+    }
+
+    public function setForca($forca)
+    {
+        $this->forca = $forca;
+    }
+
+    public function getVivo()
+    {
+        return $this->vivo;
+    }
+
+    public function setVivo($vivo)
+    {
+        $this->vivo = $vivo;
+    }
 
 }
 
--- pratica4/personagem.php
+++ pratica4/personagem.php
@@ -7,8 +7,18 @@
 </head>
 <body>
     <?php
+        require_once 'classePersonagem.php';
 
-        
+        $personagem1 = new Personagem();
+            $personagem1->setNome("Liza");
+            $personagem1->setClasse("Mago");
+
+        $personagem2 = new Personagem();
+            $personagem2->setNome("Arthur");
+            $personagem2->setClasse("Guerreiro");
+
+        print_r($personagem1);
+        print_r($personagem2);
 
     ?>
 </body>
