c5cd9a358e54979914f3581b0a602b90bd776aac
VitorVenante <vitor.venante@hotmail.com>
2025-11-03 20:25:41 -0300

    classe lutador - OK

--- aula07/classeLutador.php
+++ aula07/classeLutador.php
@@ -26,19 +26,30 @@ class Lutador{
     }
 
     public function apresentar() {
-
+        echo "<br> O Lutador de agora é: {$this->getNome()},";
+        echo " direto de {$this->getNacionalidade()}.</br>";
+        echo "<br> Com seus {$this->getIdade()} anos de idade, ";
+        echo "e com {$this->getAltura()} de altura, ";
+        echo "pesando exatamente {$this->getPeso()} kg.</br>";
+        echo "<br> Atualmente, ele está com {$this->getVitorias()} Vitórias, ";
+        echo "{$this->getDerrotas()} Derrotas e ";
+        echo "{$this->getEmpates()} Empates.</br>";
     }
     public function status() {
-
+        echo "<br>{$this->getNome()}</br>";
+        echo "<br>{$this->getPeso()}</br>";
+        echo "<br>{$this->getVitorias()}</br>";
+        echo "<br>{$this->getDerrotas()}</br>";
+        echo "<br>{$this->getEmpates()}</br>";
     }
     public function ganharLuta() {
-
+        $this->setVitorias($this->getVitorias() + 1);
     }
     public function perderLuta() {
-
+        $this->setDerrotas($this->getDerrotas() + 1);
     }
     public function empatarLuta() {
-
+        $this->setEmpates($this->getEmpates() + 1);
     }
 
     // Métodos Especiais
@@ -86,7 +97,17 @@ class Lutador{
         $this->peso = $peso;
     }
     public function setCategoria($categoria) {
-        $this->categoria = $categoria;
+        if($this->getCategoria() < 52.2){
+            $this->setCategoria($this->getCategoria("Inválido Magrelo"));
+        } else if ($this->getCategoria() <= 70.3){
+            $this->setCategoria($this->getCategoria("Leve"));
+        } else if ($this->getCategoria() <= 83.9){
+            $this->setCategoria($this->getCategoria("Medio"));
+        } else if ($this->getCategoria() <= 120.2){
+            $this->setCategoria($this->getCategoria("Pesado"));
+        } else {
+            $this->setCategoria($this->getCategoria("Inválido Gordão"));
+        }
     }
     public function setVitorias($vitorias) {
         $this->vitorias = $vitorias;
--- aula07/lutador.php
+++ aula07/lutador.php
@@ -9,15 +9,24 @@
     <?php
         require_once 'classeLutador.php';
 
-        $lutador1 = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, "Leve", 11, 2, 1);
-        $lutador2 = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, "Leve", 14, 2, 3);
-        $lutador3 = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, "Médio", 12, 2, 1);
-        $lutador4 = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, "Médio", 13, 0, 2);
-        $lutador5 = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, "Pesado", 5, 4, 3);
-        $lutador6 = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, "Pesado", 12, 2, 4);
+        // $lutador1 = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, "Leve", 11, 2, 1);
+        // $lutador2 = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, "Leve", 14, 2, 3);
+        // $lutador3 = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, "Médio", 12, 2, 1);
+        // $lutador4 = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, "Médio", 13, 0, 2);
+        // $lutador5 = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, "Pesado", 5, 4, 3);
+        // $lutador6 = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, "Pesado", 12, 2, 4);
 
-        var_dump($lutador1);
+        // Óia a sacanagem kkkk
 
+        $lutador = [0,1,2,3,4,5];
+        $l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, "Leve", 11, 2, 1);
+        $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, "Leve", 14, 2, 3);
+        $l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, "Médio", 12, 2, 1);
+        $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, "Médio", 13, 0, 2);
+        $l[4] = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, "Pesado", 5, 4, 3);
+        $l[5] = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, "Pesado", 12, 2, 4);
+        
+        // $l[0]->apresentar();
 
     ?>
 </body>
