a42aef34f3b8a574dbefe7d780088d7b8e72d3c0
VitorVenante <vitor.venante@hotmail.com>
2025-11-11 19:42:36 -0300

    classeCarro definida

--- pratica6/classeCarro.php
+++ pratica6/classeCarro.php
@@ -4,10 +4,13 @@ require_once 'interfaceCarro.php';
 class Carro implements interfaceCarro{
 
     private     bool    $ligado;
+    private     int     $potencia;
+    private     string  $categoria;
 
-    public function __construct()
+    public function __construct($ligado, $categoria)
     {
         $this->ligado = false;
+        $this->setCategoria($categoria);
     }
     // métodos principais
     public function ligar()
@@ -33,10 +36,29 @@ class Carro implements interfaceCarro{
     {
         return $this->ligado;
     }
+    public function getPotencia()
+    {
+        return $this->potencia;
+    }
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
     public function setLigado($ligado)
     {
         $this->ligado = $ligado;
     }
+    public function setPotencia($potencia)
+    {
+        $this->potencia = $potencia;
+        $this->setCategoria();
+    }
+    public function setCategoria()
+    {
+        
+    }
+
 }
 
 ?>
